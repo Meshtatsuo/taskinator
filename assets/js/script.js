97cd79cabc209dfd424ed2b63ab5d42d7f5e167c
MesH <meshtatsuo@gmail.com>
2022-01-11 16:45:05 -0700

    form input functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,28 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 let addTaskHandler = function () {
   event.preventDefault();
-  let taskItemEl = document.createElement("li");
-  let taskTextEl = document.querySelector;
-  console.log(taskTextEl);
-  taskItemEl.textContent = taskTextEl;
-  taskItemEl.className = "task-item";
 
+  // create list item
+  let taskItemEl = document.createElement("li");
+  taskItemEl.className = "task-item";
+  let taskNameEl = document.querySelector("input[name='task-name']").value;
+
+  // create div to hold task info and add to list item
+  let taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  let taskCatEl = document.querySelector("select[name='task-type']").value;
+
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameEl +
+    "</h3><span class='task-type'>" +
+    taskCatEl +
+    "</span>";
+  //Append HTML to new LI element
+  taskItemEl.appendChild(taskInfoEl);
+
+  // Add new element to page
   tasksToDoEl.appendChild(taskItemEl);
 };
 
